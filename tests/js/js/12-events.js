bbc9f5246d3b6c132b13b5492014446071adf9e4
yvan <y.douenel@coopernet.fr>
2021-03-25 16:01:48 +0100

    exo sur hidden en cours.

--- tests/js/js/12-events.js
+++ tests/js/js/12-events.js
@@ -1,23 +1,21 @@
-//console.log('this : ', this);
-//console.log('window : ', window);
 // Récupération de l'élément du dom qui correspond à mon unique bouton
+const h1 = document.querySelector("h1");
 const p = document.querySelector("p");
-
-//console.log('propriété onclick du bouton : ', button.onclick);
-let cpt = 0;
-
-// Affecter une référence à une fonction gestionnaire d'événement à mon bouton
-// Attention à ne pas affecter l'appel d'une fonction (synchrone)
-// A chaque click sur le bouton, la fonction gestionnaire d'événement
-// sera appelée
-p.onclick = function(event) {
-  cpt ++;
-  console.log('Click sur le p', cpt);
-  console.log('this : ', this);
-  console.log('event.target', event.target);
-}
-//console.log('p.onclick : ', p.onclick);
 
 // Faire en sorte que le click sur le titre supprime ou cache
 // le paragraphe
+// 1 : récupérer la référence du titre de niveau 1
+// 2 : affecter à l'événement click du titre une fonction
+// 3 : cette fonction récupère l'élément du dom qui correspond au paragraphe
+// 4 : cacher ou supprimer le paragraphe
+h1.onclick = function() {
+  // opérateur ternaire
+  //p.hidden = p.hidden ? false : true;
+  
+ /*  if(p.hidden == true) p.hidden = false;
+  else p.hidden = true; */
+  xxxx.classList.toggle("hidden");
+}
 
+// Faire fonctionner ce code avec element.classList.toggle("hidden");
+// Cela implique que vous ayez écrit le code css .hidden {display: none;}
