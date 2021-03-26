a3ebb991a3b50cd7dde7ec384e1efe20e51ebb2f
yvan <y.douenel@coopernet.fr>
2021-03-26 17:11:31 +0100

    exo fetch en cours.3

--- exos/js/js/03-fetch.js
+++ exos/js/js/03-fetch.js
@@ -9,53 +9,43 @@ fetch("https://www.coopernet.fr/rest/session/token/")
     console.log("Token récupéré : ", data);
     // Maintenant que j'ai le token, je peux afficher le formulaire
     document.querySelector("#form-user").classList.remove("hidden");
-    
+
     // Appel de getUser
-    return  getUser(data,"y","y");
+    return getUser(data, "y", "y");
   })
-  .then(function (data) { 
+  .then(function (data) {
     // J'ai les données de l'utilisateur
     console.log('Données utilisateur : ', data);
   })
   .catch(error => { console.log("Erreur attrapée : ", error.message) });
 
-  function successGetUser() {
-    console.log('Dans successGetUser');
-  }
-  function failGetUser() {
-    console.log('Dans failGetUser');
-  }
 
-  // Appel de getUser
- 
-
-
- function getUser (token,login,pwd) {
-    // création de la requête
-    console.log("Dans getUser de coopernet.");
-    return fetch("https://www.coopernet.fr/user/login?_format=json", {
-      credentials: "same-origin",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-CSRF-Token": token
-      },
-      body: JSON.stringify({
-        name: login,
-        pass: pwd
-      })
+function getUser(token, login, pwd) {
+  // création de la requête
+  console.log("Dans getUser de coopernet.");
+  return fetch("https://www.coopernet.fr/user/login?_format=json", {
+    credentials: "same-origin",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRF-Token": token
+    },
+    body: JSON.stringify({
+      name: login,
+      pass: pwd
     })
-      .then(response => response.json())
-      .then(data => {
-        //console.log("success", data);
-        if (data.current_user === undefined) {
-          console.log("Erreur de login");
-          
-          throw new Error("Erreur de data : ", data);
-        } else {
-          return data;
-        }
-      })
-      .catch(error => { console.error("Erreur attrapée dans tokenSuccess", error) });
+  })
+    .then(response => response.json())
+    .then(data => {
+      //console.log("success", data);
+      if (data.current_user === undefined) {
+        console.log("Erreur de login");
 
-  };
+        throw new Error("Erreur de data : ", data);
+      } else {
+        return data;
+      }
+    })
+    .catch(error => { console.error("Erreur attrapée dans tokenSuccess", error) });
+
+};
