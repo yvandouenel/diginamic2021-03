a8bcc2bcf3cb2c7b683782eadee043bdee548784
yvan <y.douenel@coopernet.fr>
2021-03-30 17:31:50 +0200

    Exo memo en cours : qui est this ?

--- exos/memo/App.js
+++ exos/memo/App.js
@@ -5,33 +5,58 @@ import FetchData from "./services/FetchData.js";
 class App extends DomUtils {
   constructor() {
     super();
+    // Propriétés de base
     this.token = "";
     this.user = {};
     this.terms = [];
     this.columns = [];
 
+    // Création et récupération des élements de base du dom
     this.domElements = this.render();
 
-    // instanciation du service FetchData
+    // Instanciation du service FetchData et récupération du token
     this.fd = new FetchData();
+    this.getTokenAndUser();
+
+
+  }
+
+  getTokenAndUser() {
     this.fd.getToken()
-    .then((data) => {
-      this.token = data;
-      console.log('this.domElements.form_login : ', this.domElements.form_login);
-      this.domElements.form_login.classList.remove("hidden");
-    })
-    .catch((error) => {
-      console.error('Erreur catché dans getToken : ' + error.message);
-    });
-    //this.fd.getToken();
+      .then((data) => {
+        this.token = data;
+        console.log('this.domElements.form_login : ', this.domElements.form_login);
+        this.domElements.form_login.classList.remove("hidden");
+        // J'ai le token et j'ai affiché le formulaire, donc je peux aller
+        // Chercher les données de l'utilisateur
+        this.domElements.form_login.onsubmit = function (event) {
+          event.preventDefault();
+          const login = document.getElementById("login").value;
+          const pwd = document.getElementById("pwd").value;
+          // Qui est this ?
+          console.log('this : ', this);
+          /* this.fd.getUser(this.token, login, pwd)
+          .then((data) => {
+            console.log('user dans getTokenAndUser : ', data);
+          })
+          .catch((error) => {
+            console.error("Erreur attrapée dans getTokenAndUser : " + error.message)
+          }) */
+        }
+
+
+      })
+      .catch((error) => {
+        console.error('Erreur catché dans getToken : ' + error.message);
+      });
   }
 
   render() {
     // Création des éléments de base du dom
-    const header = this.createCompleteDomElement("header","",[], document.getElementById("app"));
-    const main = this.createCompleteDomElement("main","",[], document.getElementById("app"));
-    const footer = this.createCompleteDomElement("footer","",[], document.getElementById("app"));
-    const form_login = new Form(main,{
+    const header = this.createCompleteDomElement("header", "", [], document.getElementById("app"));
+    const main = this.createCompleteDomElement("main", "", [], document.getElementById("app"));
+    const footer = this.createCompleteDomElement("footer", "", [], document.getElementById("app"));
+    const form_login = new Form(main, {
       inputs: [
         {
           label: {
--- exos/memo/services/FetchData.js
+++ exos/memo/services/FetchData.js
@@ -17,4 +17,32 @@ export default class FetchData {
         return data;
       })
   }
+
+  getUser(token, login, pwd) {
+    // création de la requête
+    console.log("Dans getUser de FetchData");
+    // Va chercher sur le point d'entrée :
+    // https://www.coopernet.fr/user/login?_format=json 
+    return fetch("https://www.coopernet.fr/user/login?_format=json", {
+      credentials: "same-origin",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": token
+      },
+      body: JSON.stringify({
+        name: login,
+        pass: pwd
+      })
+    })
+      .then(response => response.json()) // Teste si la réponse est bien du json
+      .then(data => {
+        //console.log("success", data);
+        if (data.current_user === undefined) {
+          throw new Error("Erreur de login");
+        } else {
+          return data;
+        }
+      })
+  };
 }
