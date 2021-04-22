b79a5a8452ab6bb5c51d9780742be7873f457b28
yvan <y.douenel@coopernet.fr>
2021-04-22 15:01:54 +0200

    Premiers test php.

---
+++ php/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Test apache</title>
+</head>
+<body>
+  <h1>Apache répond !!!</h1>
+  <?php 
+  echo phpinfo();
+  ?>
+</body>
+</html>
